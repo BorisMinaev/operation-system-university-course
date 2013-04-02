8632f89545bde4e2c44bf06d6897ab55090bea2c
Boris Minaev <qwerty787788@qwerty787788.(none)>
2013-04-03 03:16:20 +0400

    simple read-write

--- dupme/main.c
+++ dupme/main.c
@@ -1,4 +1,20 @@
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    write(1, "Hello world!\n", 13);
+    if (argc != 2) {
+        return -1;
+    }
+    int k = atoi(argv[1]);
+    char *buffer = (char *) malloc(k + 1);
+    while (1) {
+        int r = read(0, buffer, k);
+        if (r == 0) {
+            break;
+        }
+        *(buffer + r) = '\n';
+        write(1, buffer, r + 1);
+    }
+    free(buffer);
+    return 0;
 }
+
