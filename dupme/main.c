8fc6bcb53859e6d5dded89128285837897ffa6ad
Boris Minaev <qwerty787788@qwerty787788.(none)>
2013-04-03 15:02:59 +0400

    changed <k to <=k

--- dupme/main.c
+++ dupme/main.c
@@ -3,7 +3,7 @@ int main(int argc, char** argv) {
     if (argc != 2) {
         return -1;
     }
-    int k = atoi(argv[1]);
+    int k = atoi(argv[1]) + 1;
     char *buffer = (char *) malloc(k);
     int from = 0;
     int now_write = 1;
