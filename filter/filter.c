d2399aff0d7fd5e40f81b553c4d2894c74bdd368
Boris Minaev <qwerty787788@qwerty787788.(none)>
2013-04-12 16:45:41 +0400

    getopt parsing

--- filter/filter.c
+++ filter/filter.c
@@ -5,18 +5,38 @@ int buf_len = 4 * 1024;
 char *buffer;
 
 void do_main_part(int fr, int len) {
-
+    write(1, buffer + fr, len);
 }
 
 int main(int argc, char ** argv) {
-    buffer = (char *) mallok(buf_len);
+    int c;
+    while ((c = getopt(argc, argv, "nzb:")) != -1) {
+        switch (c)
+        {
+            case 'n':
+                delim = '\n';
+                break;
+            case 'z':
+                delim = 0;
+                break;
+            case 'b':
+                buf_len = optarg;
+                break;
+            default:
+                abort();
+        }
+    }
+
+
+
+    buffer = (char *) malloc(buf_len);
     int eof = 0;
     int from = 0;
     while (1 - eof) {
         int r = read(0, buffer + from, buf_len - from);
         if (r == 0) {
             eof = 1;
-            *(biffer + from) = delim;
+            *(buffer + from) = delim;
             from++;
         }    
         from += r;
@@ -32,8 +52,8 @@ int main(int argc, char ** argv) {
             if (first_delim == -1) {
                 break;
             }
-            do_main_part(0, first_delim);
-            for (int i = first_delim + 1; i < from; i++) {
+            do_main_part(0, first_delim - 1);
+            for (i = first_delim + 1; i < from; i++) {
                 *(buffer + i - first_delim - 1) = *(buffer + i);
             }
             from -= first_delim - 1;
