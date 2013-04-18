4e6af0626c16804ed5dc149c086765dada00e6c4
Boris Minaev <qwerty787788@qwerty787788.(none)>
2013-04-19 03:08:49 +0400

    main part

--- simple-bash/watchthis.c
+++ simple-bash/watchthis.c
@@ -13,7 +13,6 @@ void remove_files() {
 
 void my_exit() {
     remove_files();
-    puts("by!");
     _exit(0);
 }
 
@@ -25,9 +24,38 @@ int main(int argc, char ** argv) {
     signal(SIGINT, my_exit);
     signal(SIGTERM, my_exit);
 
+    if (argc < 3) {
+       puts("Need more arguments!");
+       my_exit(); 
+    }
+    int time_to_sleep = atoi(argv[1]);
+
     while (1) {
-       sleep(1);
-      puts("Hello, world");
+        int cur_file = open(cur, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
+        int child = 0;
+        if ((child = fork()) == 0) {
+            dup2(cur_file, 1);
+            close(cur_file);
+            execvp(argv[2], argv+ 2);
+            _exit(1);
+        }
+        waitpid(child, NULL, 0);
+        if ((child = fork()) == 0) {
+            execlp("cat", "cat", cur, NULL);
+            _exit(1);
+        } 
+        waitpid(child, NULL, 0);
+        if ((child = fork()) == 0) {
+           execlp("diff", "diff", "-u", last, cur, NULL);
+           _exit(1);
+        }
+        waitpid(child, NULL, 0); 
+        if ((child = fork()) == 0) {
+            execlp("mv", "mv", cur, last, NULL);
+            _exit(1);
+        }
+        waitpid(child, NULL, 0);
+        sleep(time_to_sleep);
     } 
 
     remove_files();
