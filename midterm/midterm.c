f58efac295a7449a2d9435577a1f02c9a2770176
Boris Minaev <qwerty787788@qwerty787788.(none)>
2013-04-19 19:23:22 +0400

    alot of code added

--- midterm/midterm.c
+++ midterm/midterm.c
@@ -3,13 +3,19 @@
 #include <fcntl.h>
 
 int buf_len = 100;
+char * buffer_to_run;
 char * buffer;
+char * string_to_write;
 int file_r = -1;
+int file_to_run = -1;
 
 void my_exit(int exit_code) {
     free(buffer);
+    free(buffer_to_run);
     if (file_r != -1)
         close(file_r);
+    if (file_to_run != -1)
+        close(file_to_run);
     _exit(exit_code);
 }
 
@@ -32,22 +38,18 @@ void my_print(char * s, int len) {
     }
 }
 
-int main(int argc, char ** argv) {
-    if (argc != 4) {
-        puts("Usage: midterm filename string_number new_string");
-        _exit(1);
-    }
-    buffer = (char *) malloc(buf_len);
-    file_r = open(argv[1], O_RDONLY);
+char * file_to_change;
+
+void do_main_part(int string_number) {
+    file_r = open(file_to_change, O_RDONLY);
     if (file_r == -1) {
-        puts("Error while opening file");
+        puts("Error while reading file");
         my_exit(1);
     }
     int lines_read = 0;
-    int string_number = atoi(argv[2]);
     string_number--;
     if (string_number == 0) {
-        my_print(argv[3], str_len(argv[3]));
+        my_print(string_to_write, str_len(string_to_write));
         write(1, "\n", 1);
     }
     while (1) {
@@ -62,7 +64,7 @@ int main(int argc, char ** argv) {
                 my_print(buffer + st, i - st + 1);
                 st = i + 1;
                 if (lines_read == string_number) {
-                    my_print(argv[3], str_len(argv[3]));
+                    my_print(string_to_write, str_len(string_to_write));
                     write(1, "\n", 1);
                 }
             }
@@ -72,8 +74,59 @@ int main(int argc, char ** argv) {
         }
     }
     if (lines_read < string_number) {
-        my_print(argv[3], str_len(argv[3]));
+        my_print(string_to_write, str_len(string_to_write));
         write(1, "\n", 1);
     }
+    close(file_r);
+    file_r = -1;
+}
+
+void work_with_param(char * s, int len) {
+    my_print(s, len);
+}
+
+int main(int argc, char ** argv) {
+    if (argc != 4) {
+        puts("Usage: midterm filename_to_run filename_to_change new_string");
+        _exit(1);
+    }
+    buffer = (char *) malloc(buf_len);
+    buffer_to_run = (char *) malloc(buf_len);
+    file_to_run = open(argv[1], O_RDONLY);
+    file_to_change = argv[2];
+    string_to_write = argv[3];
+    if (file_to_run == -1) {
+        puts("Error while opening file");
+        my_exit(1);
+    }
+    int already = 0;
+    int eof = 0;
+    while (1) {
+        int re = read(file_to_run, buffer_to_run + already, buf_len - already);
+        if (re < 0) 
+            my_exit(1);
+        if (re == 0)
+            eof = 1;
+        already += re;
+        while (1) {
+            int first_endl = -1;
+            int i;
+            for (i = 0; i < already; i++) 
+                if ((*(buffer_to_run + i)) == '\n') {
+                    first_endl = i;
+                    break;
+                }
+            if (first_endl == -1 && already == buf_len) 
+                my_exit(1);
+            if (first_endl != -1) {
+                work_with_param(buffer_to_run, first_endl
+                for (int i = first_endl + 1; i < already; i++)
+                    *(buffer + i - first_endl - 1) = *(buffer + i);
+            } else {
+                break;
+            } 
+        }
+        if (eof) break;
+    } 
     my_exit(0);
 }
