1b9ab7eba675c20dfde550845d876ee37252effa
Boris Minaev <qwerty787788@qwerty787788.(none)>
2013-04-19 16:19:05 +0400

    my_ext function

--- midterm/midterm.c
+++ midterm/midterm.c
@@ -1,6 +1,15 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <signal.h>
+
+int buf_len = 100;
+char * buffer;
+
+void my_exit(int exit_code) {
+    free(buffer);
+    _exit(exit_code);
+}
 
 int string_len(char * s) {
     int res = 1;
@@ -16,7 +25,7 @@ void my_print(char * s, int len) {
     while (alr < len) {
        int add_len = write(1, s, len - alr);
        if (add_len < 0)
-           _exit(1);
+           my_exit(1);
        alr += add_len;
     }
 }
@@ -26,6 +35,11 @@ int main(int argc, char ** argv) {
         puts("Usage: midterm filename string_number new_string");
         _exit(1);
     }
+    buffer = (char *) malloc(buf_len);
     char * str_test = "abcd\n";
     my_print(str_test, 5);
+
+
+
+    my_exit(0);
 }
